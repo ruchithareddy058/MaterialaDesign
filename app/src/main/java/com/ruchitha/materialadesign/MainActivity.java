dcd87f4f7a7245975e7d99bb0134b948907e9792
ruchithareddy058 <ruchithareddy058@gmail.com>
2021-06-20 11:38:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/ruchitha/materialadesign/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ruchitha.materialadesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void MaterialDesign(View view) {
+        startActivity(new Intent(this,MyMaterialDesign.class));
+    }
+}
---
+++ app/src/main/java/com/ruchitha/materialadesign/MyMaterialDesign.java
@@ -0,0 +1,14 @@
+package com.ruchitha.materialadesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyMaterialDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_material_design);
+    }
+}
